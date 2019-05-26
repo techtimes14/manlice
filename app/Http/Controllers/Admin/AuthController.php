c4a157fec4d54e3a48570fbae47cd769a8d0062d
mitkarghosh <100websolution@gmail.com>
2019-05-26 16:10:17 +0530

    4th

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -8,9 +8,6 @@ use Auth;
 
 use Hash;
 
-use App\Model\AdminPermision;
-use App\Model\AdminMenu;
-
 //use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthController extends CommonController
@@ -49,7 +46,7 @@ class AuthController extends CommonController
                 if (Auth::guard('admin')->attempt(array_merge($request->only('email','password'), ['user_type' => ['A','AG']]))) {
 
                     $user_id = Auth::guard('admin')->user()->id;
-                    $this->put_session($user_id);
+                    //$this->put_session($user_id);
 
                     return redirect()->route('admin.dashboard');
                 }else{
@@ -64,19 +61,6 @@ class AuthController extends CommonController
         return view('admin.auth.login');
     }
 
-    /* For permission session store */
-    public function put_session($user_id = null){
-        Session::put('permissions.user_type', Auth::guard('admin')->user()->user_type);
-        $permision = AdminPermision::where('user_id',$user_id)->get();
-        if(count($permision) >0){
-            foreach ($permision as $key => $menus) {
-                $k=$menus->admin_menu;
-                $controller_method=strtolower($k['controller_name']).'.'.strtolower($k['method_name']);
-                Session::put('permissions.'.$controller_method, 1);
-            }
-        }
-    }
-                    
     /**
      * Show the admin dashboard.
      *
